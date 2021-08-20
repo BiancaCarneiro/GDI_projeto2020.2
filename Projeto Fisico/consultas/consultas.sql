354cdc6172c4ad298a1b5d003568453ec437c6e1
BiancaCarneiro <bianca.ccnf@hotmail.com>
2021-08-20 12:16:03 -0300

    leve modificacao

--- Projeto Fisico/consultas/consultas.sql
+++ Projeto Fisico/consultas/consultas.sql
@@ -1,14 +1,13 @@
 --group by/having
 --Pega as desenvolvedoras que tem mais jogos que a media
-SELECT a.name as 'Nome', AVG(a.qtd_jogos)
-FROM DESENVOLVEDORA
+SELECT AVG(a.qtd_jogos), a.name as "Nome"
+FROM DESENVOLVEDORA a
 GROUP BY a.name
 HAVING a.qtd_jogos > AVG(a.qtd_jogos)
 
-
 --Junção interna
 --Pega o nome do empregado e do seu chefe
-SELECT a.nome as 'funcionario', b.name as 'chefes'
+SELECT a.nome as "funcionario", b.name as "chefes"
 FROM EMPREGADO a
 INNER JOIN EMPREGADO b
 ON a.cpf = b.cpf_chefe
@@ -16,7 +15,7 @@ ON a.cpf = b.cpf_chefe
 
 --semi join
 --Seleciona os empregados que são chefes de alguém
-SELECT a.name as 'Chefe'
+SELECT a.name as "Chefe"
 FROM EMPREGADO a
 WHERE EXISTS (SELECT 1 
               FROM EMPREGADO b
@@ -26,7 +25,7 @@ WHERE EXISTS (SELECT 1
 
 --anti join
 --Seleciona os funcionários que não são chefes
-SELECT a.name as 'Nao eh chefe'
+SELECT a.name as "Nao eh chefe"
 FROM EMPREGADO a
 WHERE NOT EXISTS (SELECT 1 
                   FROM EMPREGADO b
@@ -36,6 +35,6 @@ WHERE NOT EXISTS (SELECT 1
 
 --where
 --Seleciona a desenvolvedora que tem pelo menos um jogo
-SELECT a.name as 'Desenvolvedoras com jogos'
+SELECT a.name as "Desenvolvedoras com jogos"
 FROM DESENVOLVEDORA a
 WHERE a.qtd_jogos > 0
