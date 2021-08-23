4097362c87aab18b0c8e5c7084ae7d5f2d2e9877
aaavalenca <aaav@cin.ufpe.br>
2021-08-23 15:31:49 -0300

    adicionando algumas consultas PL, um procedure e uma função

--- Projeto Fisico/pl/ConsultasPL.sql
+++ Projeto Fisico/pl/ConsultasPL.sql
@@ -1,67 +1,85 @@
---Trigger para adicionar em qtd jogos quando um jogo for inserido
+-- CONSULTAS PL ANÔNIMAS
 
-create or replace trigger  adicionaJOGOemDEV
-    after insert on JOGO
-    for each row
-DECLARE 
-    ID_DEV NUMBER;
-begin
-    UPDATE DESENVOLVEDORA d
-    SET d.QTD_JOGOS = d.qtd_jogos +1
-    WHERE :new.id = d.ID;
-end;
+-- Declarando variável do tipo linha, atribuindo-lhe valores
+-- e imprimindo um deles, concatenando com texto
+SET SERVEROUTPUT ON
+DECLARE
+    -- declaracao de variavel
+    reg_estagiario estagiario%ROWTYPE;
+BEGIN
+    reg_estagiario.cpf := '09302028488';
+    reg_estagiario.bolsa := 2000;
+    reg_estagiario.faculdade := 'UFPE';
+    DBMS_OUTPUT.PUT_LINE('CPF: '|| reg_estagiario.cpf);
+END;
+
+-- Imprime quantidade de empregados usando select-into para
+-- inserir valor em variável a partir de consulta
+SET SERVEROUTPUT ON
+DECLARE
+    qtd_emp number;
+BEGIN
+    select count(CPF) into qtd_emp from empregado;
+    DBMS_OUTPUT.PUT_LINE('Qtd de empregados: '|| qtd_emp);
+END;
+
+-- &caractere pega input do usuário. Esta consulta
+-- imprime o caractere inserido em minúscula
+SET SERVEROUTPUT ON
+DECLARE
+    x CHAR(2) := LOWER('&caractere');
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('virando maiuscula: '|| x);
+END;
+
+-- PROCEDURES
+
+-- Procedure parametrizado, sem retorno, com uso de cursor
+-- (lista todos os jogos de uma determinada desenvolvedora)
+create or replace PROCEDURE list_jogos_des (desnum number) IS
+    -- insere o codigo da desenvolvedora
+    id_des desenvolvedora.id%type := (desnum);
+    des_name desenvolvedora.name%type;
+    jogo_des jogo.name%type;
+    i number := 1;
+    CURSOR cur IS
+        select name from jogo where id = id_des;
+BEGIN
+    select MAX(name) into des_name from desenvolvedora where id = id_des;
+    OPEN cur;
+    LOOP
+        FETCH cur into jogo_des;
+        EXIT WHEN cur%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Jogo ' || i || ' da desenvolvedora '|| des_name || ': ' || jogo_des);
+        i := i + 1;
+    END LOOP;
+    CLOSE cur;
+END;
+
+-- Chamando procedure
+SET SERVEROUTPUT ON
+BEGIN
+    list_jogos_des(123);
+END;
 
 
--- AS CONSULTAS ABAIXO NAO FORAM TESTADAS
+-- FUNÇÕES
 
--- Gatilho
--- Muda o valor da compra quando há promoção
-create or replace trigger checaValorCompra
-    after insert on ELEGIVEL_PROM
-    for each row
-declare 
-    desconto NUMBER;
-    cursor cur_val is (
-        SELECT p.percent_desconto
-        FROM PROMOCAO p
-        WHERE new.id = p.id 
-    );
-begin
-    open cur_val;
-    loop
-        fetch cur_val into desconto;
-        EXIT WHEN cur_val%notfound;
-    UPDATE COMPRA c;
-    SET c.valor = c.valor*desconto;
-    WHERE :new.(email, id, codigo) = c.(email, id, codigo);
-end;
+-- Função com retorno de número e uso de exceção
+-- (devolve todos os consumidores cadastrados no steam)
+create or replace FUNCTION qtd_consumidores RETURN NUMBER IS
+    qtdC number;
+BEGIN
+    select COUNT(email) into qtdC from consumidor;
+    return qtdC;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            return null;
+END;
 
--- Funcao com SQL embutida e parametro
--- Pega a diferenca do salario dos funcionarios
-create or replace function getDif(cpf1 in varchar2(11 CHAR), cpf2 in varchar2(11 CHAR))    
-return number    
-is     
-  dif number;  
-  sal1 number;
-  sal2 number;  
-begin    
-    SELECT salario INTO sal1
-    FROM SENIOR
-    WHEre cpf = cpf1;
+-- Chamando função
 
-    SELECT salario INTO sal2
-    FROM SENIOR
-    WHEre cpf = cpf2;
-
-    dif := ABS(sal1-sal2)
-  return dif;    
-end;
-
-
--- procedimento com parametros
--- cria uma compra
-CREATE OR REPLACE PROCEDURE add_compra(email in VARCHAR2, id in NUMBER, codigo in NUMBER, valor in NUMBER, meio_de_pag IN VARCHAR2)
-IS 
-   BEGIN 
-    insert into compra values(email, id, codigo, valor, meio_de_pag);
-  END;
+SET SERVEROUTPUT ON
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(qtd_consumidores);
+END;
